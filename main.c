b9e375f55150254c162a28077280e1fba577743c
Basem Rizk <basem.rizk@outlook.com>
2018-12-07 01:04:02 +0100

    Recognition Logic and some comments (Code exceeding EVAL edition limit)

--- main.c
+++ main.c
@@ -20,20 +20,26 @@ void T1_ISR(void) interrupt 3 {
 	TF1 = 0; // Reset the interrupt request
 }
 
-int i=0;
+int key_i=0;
 
 // Testing by use of word ".tie5Ronal"
-char key[11] = {'.', 't', 'i', 'e', '5', 'R', 'o', 'n', 'a', 'l', '\0'};
+//char key[11] = {'.', 't', 'i', 'e', '5', 'R', 'o', 'n', 'a', 'l', '\0'};
 char test[11] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\0'};
 
-// Array to save time intervals between each user press
+// Arrays to save time intervals between each user press
 unsigned int time_between_chars_typed_userA[10];
 unsigned int time_between_chars_typed_userB[10];
 unsigned int time_between_chars_typed_recognize[10];
 
 
-int trials_num = 5;
-int states_bit = 0;		//0 means userA training phase, 1 means userB training phase, 2 means entering test phase  
+unsigned char trials_num = 5;	// # of entries needed to be entered by current user
+
+// state_bit
+// 0 :userA training phase
+// 1 :means userB training phase
+// 2 :means entering test phase  
+unsigned char states_bit = 0;
+
 	
 void main (void) {
 	/*Note : timer takes 135 micro seconds to overflow*/
@@ -56,53 +62,90 @@ void main (void) {
 	--------------------------------------*/
 	ET1 = 1;				// Setting Timer 1 interrupt
 	EA = 1;	
-	// Allocating space for the data to be saved
-	//int r = 9;	// Number of observed calculation
-	//int c = 2;	// Number of users
-	//float *features = (float *)malloc(r* sizeof(float));
-	printf("User A enters Sequance 1234567890 5 times\n");
-	
+
+
+	// Program Logic follows
+	printf("User A, Please enter the following Sequance 5 times:\n '1234567890' \n");
 	
 	while (1) {
+		
 		char x = _getkey();
+		printf("#%d Entry", trials_num);
 		
 		if(states_bit == 0 && trials_num == 0){
-			trials_num = 5;   //Setting number of trials for user B training 
-			states_bit = 1;		//finished training user A and entering training user B
+			// Just got last entry by User A @(Training Session)
+			trials_num = 5;   // Reset # of needed entries for (Training Session) by User B
+			states_bit = 1;		// Set to User B (Training Session) 
+		
+		} else if(states_bit == 1 && trials_num == 0 ) {
+			// Just got last entry by User B @(Training Session)
+			states_bit = 2; // Set to (Test Session)
+			
 		}
-		else if(states_bit == 1 && trials_num == 0)
-			states_bit = 2;		//finished training user B and entering test phase
 		
 		
-		if (x == test[i]) {
-			if (i != 0 && states_bit == 0) {
-				time_between_chars_typed_userA[i-1] = T1_ISR_count;
-				printf("%u\n", time_between_chars_typed_userA[i-1]);
-			}
-			else if(i != 0 && states_bit == 1) {
-				time_between_chars_typed_userB[i-1] = T1_ISR_count;
-				printf("%u\n", time_between_chars_typed_userB[i-1]);	
-			}
-			i++;
+		if (x == test[key_i]) {
+			// a Key was pressed Logic
+			if (key_i != 0 && states_bit == 0) {
+				time_between_chars_typed_userA[key_i-1] = T1_ISR_count;
+				printf("%u\n", time_between_chars_typed_userA[key_i-1]);
+			
+			} else if(key_i != 0 && states_bit == 1) {
+				time_between_chars_typed_userB[key_i-1] = T1_ISR_count;
+				printf("%u\n", time_between_chars_typed_userB[key_i-1]);	
+				
+			} else {
+				time_between_chars_typed_recognize[key_i-1] = T1_ISR_count;
+				printf("%u\n", time_between_chars_typed_recognize[key_i-1]);	
+				
+			}				
+			key_i++;
 			T1_ISR_count = 0;				
+	
+		} else { 
+			printf("Another key pressed; Repeat from the beginning of the sequence.\n");
+			key_i = 0;
+			// TODO does the counter reset in this case?
 		}
 		
 		
-		 else { 
-			printf("Another Key Pressed\n");
-			i = 0;
-		}
-		
-		if (i == 10) {
-			i = 0;
+		if (key_i == 10) {
+			// Sequence entered correctly Logic
+			key_i = 0;
+			
 			if(states_bit == 0 || states_bit == 1)
+				// It is a (Training Session)
 				trials_num --;
+			
 			if(states_bit == 0 && trials_num == 0){
-				printf("user A finished training phase\nNow user B enters 1234567890 5 times\n");
+				printf("User A finished training phase\n");
+				printf("Now User B, Please enter the following Sequance 5 times:\n '1234567890' \n");
+				
+			} else if(states_bit == 1 && trials_num == 0){
+				printf("User B finished training phase\n");
+				printf("Now any user enters 1234567890 and wait for detection\n");
+		
+			} else if(states_bit == 2) {
+				// Detection Logic
+				unsigned char feature_i = 0;
+				unsigned short error_sum = 0;
+				
+				for(; feature_i < 10; feature_i++) {
+		
+					error_sum += (time_between_chars_typed_recognize[feature_i] - time_between_chars_typed_userA[feature_i]);
+					error_sum -= (time_between_chars_typed_recognize[feature_i] - time_between_chars_typed_userB[feature_i]);
+				}
+				
+				if(error_sum > 0) {
+					printf("This was probably User B.");
+				} else if (error_sum < 0) {
+					printf("This was probably User A.");
+				} else {
+					printf("Oh! no I can not take a guess!");
+				}
+				
 			}
-			else if(states_bit == 1 && trials_num == 0){
-				printf("user B finished training phase\nNow any user enters 1234567890\n");
-			}
+			
 		}
 	}
 }
